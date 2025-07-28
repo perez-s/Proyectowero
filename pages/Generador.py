d0c7a69842642ce8f77ba0b5a44b01bd7af01721
perez-s <perez14sebastian@gmail.com>
2025-07-28 13:50:11 -0500

    Added recommendations functionality to the Generador.py file, allowing users to select multiple recommendations and add an additional custom recommendation. The code also ensures that the recommendations are formatted correctly for output in the generated report.

--- pages/Generador.py
+++ pages/Generador.py
@@ -94,13 +94,21 @@ if ss["authentication_status"]:
     nodoc = st.text_input("Número del documento")
     obsevacion = st.text_area("Observaciones adicionales")
     obsevacion2 = repr(obsevacion).replace("'", '"')
+    norecomendacion = st.checkbox("Sin recomendación", value=False, key="norecomendacion")
     recomendaciones = {
-        "Sin recomendación": "",
-        "Recomendación  1": f"Desde {startdate} hasta {enddate}, se ha observado una continuidad en la presencia de grandes cantidades de material no aprovechable o material de rechazo en los puntos de recolección. Entre estos residuos, se incluyen plásticos contaminados con grasas y restos orgánicos de las proteínas, restos de comida preparada y no preparada, servilletas de cocina y de mesa, así como una mezcla de materiales reciclables con no reciclables que afecta la calidad del material recuperable. Esta situación limita la eficiencia del proceso de reciclaje y representa un desafío significativo en la correcta segregación de residuos.\n\nAdemás, los nuevos puntos de recolección implementados como parte del piloto, junto con los retos inherentes a la separación en la fuente de los residuos sólidos, evidencian la necesidad urgente de reforzar las estrategias de educación y concientización. Desde el área de sostenibilidad de Qbano, se han llevado a cabo diversas acciones de comunicación y capacitación a través de WERO, lo que ha favorecido una mayor apropiación por parte de los usuarios de las pautas de clasificación. Estos esfuerzos han resultado en una mejora en la separación de materiales reciclables y no reciclables. No obstante, el desafío persiste y continúa siendo una prioridad garantizar la efectividad del sistema de recolección.\n\nPara fortalecer la separación en la fuente en cada punto de venta de las distintas ciudades, se recomienda enfocar la estrategia en dos acciones clave- la creación de un concurso de reciclaje y la implementación de campañas de sensibilización. El concurso deberá premiar a los puntos de venta que demuestren un alto nivel de cumplimiento en la correcta separación de residuos, fomentando la motivación tanto entre los colaboradores como entre los consumidores para que se involucren activamente en la correcta separación en la fuente.\n\nAdemás, se debe ampliar el alcance de las campañas de sensibilización, utilizando medios digitales para llegar tanto a los colaboradores como a los consumidores, a través de videos tutoriales y mensajes interactivos que refuercen la importancia de la separación de residuos y su impacto positivo en el ambiente. Esta combinación de incentivos y educación podrá contribuir a una mayor conciencia y participación en el proceso de segregación en la fuente."
-        
-}
-    recomendacioneslist = st.selectbox("Selecciona una recomendación:", list(recomendaciones.keys()))
-    recomendacion2 = repr(recomendaciones[recomendacioneslist]).replace("'", '"')
+        "Recomendación rechazo": "Se ha observado una continuidad en la presencia de grandes cantidades de material no aprovechable o material de rechazo en los puntos de recolección. Entre estos residuos, se incluyen plásticos contaminados con grasas y restos orgánicos de las proteínas, restos de comida preparada y no preparada, servilletas de cocina y de mesa, así como una mezcla de materiales reciclables con no reciclables que afecta la calidad del material recuperable. Esta situación limita la eficiencia del proceso de reciclaje y representa un desafío significativo en la correcta segregación de residuos. \n\n",
+        "Recomendación fortalecimiento": "Los puntos de recolección implementados como parte del piloto, junto con los retos inherentes a la separación en la fuente de los residuos sólidos, evidencian la necesidad urgente de reforzar las estrategias de educación y concientización. \n\n",
+        "Recomendación acciones fuente": "Desde el área de sostenibilidad se han llevado a cabo diversas acciones de comunicación y capacitación a través de WERO, lo que ha favorecido una mayor apropiación por parte de los usuarios de las pautas de clasificación. Estos esfuerzos han resultado en una mejora en la separación de materiales reciclables y no reciclables. \n\n",
+        "Recomendación fortalecimiento 2": "Para fortalecer la separación en la fuente en cada punto de venta de las distintas ciudades, se recomienda enfocar la estrategia en dos acciones clave- la creación de un concurso de reciclaje y la implementación de campañas de sensibilización. El concurso deberá premiar a los puntos de venta que demuestren un alto nivel de cumplimiento en la correcta separación de residuos, fomentando la motivación tanto entre los colaboradores como entre los consumidores para que se involucren activamente en la correcta separación en la fuente. \n\n",
+        "Recomendación difusión": "Se sugiere  ampliar el alcance de las campañas de sensibilización, utilizando medios digitales para llegar tanto a los colaboradores como a los consumidores, a través de videos tutoriales y mensajes interactivos que refuercen la importancia de la separación de residuos y su impacto positivo en el ambiente. Esta combinación de incentivos y educación podrá contribuir a una mayor conciencia y participación en el proceso de segregación en la fuente. \n\n"
+    }
+    recomendacioneslist = st.multiselect("Selecciona una recomendación:", list(recomendaciones.keys()), placeholder="Escoge una o varias recomendaciones", disabled=norecomendacion)
+    recomendacionadicional = st.text_area("Recomendación adicional", placeholder="Escribe una recomendación adicional aquí...", key="recomendacionadicional", disabled=norecomendacion)
+    recomendacionfinal = "Para este periodo de informe proponemos las siguientes recomendaciones. \n\n"
+    for i in range(len(recomendacioneslist)):
+        recomendacionfinal = recomendacionfinal + f"{i+1}. {recomendaciones[recomendacioneslist[i]]}\n\n"
+    recomendacionfinal2 = recomendacionfinal + f"{len(recomendacioneslist)+1}. {recomendacionadicional}\n\n"
+    recomendacionfinal2 = repr(recomendacionfinal2).replace("'", '"')
     oganicosselect = st.selectbox("Reportar residuos orgánicos?", ["Si", "No"], index=1)
 
     if st.button("Generar Reporte", key="generate_report", use_container_width=True):
@@ -108,9 +116,9 @@ if ss["authentication_status"]:
             if os.path.exists(f"{Clientes}.pdf"):
                 os.remove(f"{Clientes}.pdf")
             if oganicosselect == "Si":
-                os.system(f"quarto render reporte_organicos.qmd -P obs:{obsevacion2} -P rec:{recomendacion2} -P nodoc:{nodoc} -P client:\"{Clientes}\" -P startdate:{startdate} -P enddate:{enddate} --output \"{Clientes}\".pdf")    
+                os.system(f"quarto render reporte_organicos.qmd -P obs:{obsevacion2} -P rec:{recomendacionfinal2} -P nodoc:{nodoc} -P client:\"{Clientes}\" -P startdate:{startdate} -P enddate:{enddate} --output \"{Clientes}\".pdf")    
             else:
-                os.system(f"quarto render reporte_base.qmd -P obs:{obsevacion2} -P rec:{recomendacion2} -P nodoc:{nodoc} -P client:\"{Clientes}\" -P startdate:{startdate} -P enddate:{enddate} --output \"{Clientes}\".pdf")
+                os.system(f"quarto render reporte_base.qmd -P obs:{obsevacion2} -P rec:{recomendacionfinal2} -P nodoc:{nodoc} -P client:\"{Clientes}\" -P startdate:{startdate} -P enddate:{enddate} --output \"{Clientes}\".pdf")
             try:    
                 with open(f"{Clientes}.pdf", "rb") as file:
                     st.download_button(
